d12078ec7d651c92ead20ed2d18f71f97d27dbb7
Eugen <vasilenkoevgen88@gmail.com>
2023-06-27 08:40:03 +0300

    Minor

--- src/App.tsx
+++ src/App.tsx
@@ -1,34 +1,24 @@
 import React from 'react';
-import Todolist from './Todolist';
 import './App.css';
+import {Todolist} from './Todolist';
 
 function App() {
-    const truck0='What to lern1'
-    const truck1='What to lern22'
-    const truck2=100200
-    const truck3=true
 
     const tasks1 = [
         { id: 1, title: "HTML&CSS", isDone: true },
         { id: 2, title: "JS", isDone: true },
-        { id: 3, title: "ReactJS", isDone: false },
-        { id: 4, title: "ReactJS", isDone: false },
-        { id: 5, title: "ReactJS", isDone: false },
-        { id: 6, title: "ReactJS", isDone: false },
-        { id: 7, title: "ReactJS", isDone: false },
-        { id: 8, title: "ReactJS", isDone: false },
-        { id: 9, title: "ReactJS", isDone: false },
-        { id: 10, title: "ReactJS", isDone: false },
+        { id: 3, title: "ReactJS", isDone: false }
     ]
     const tasks2 = [
         { id: 1, title: "Hello world", isDone: true },
         { id: 2, title: "I am Happy", isDone: false },
         { id: 3, title: "Yo", isDone: false }
     ]
+
     return (
         <div className="App">
-            <Todolist truck={truck0} truck2={truck2} tasks={tasks1}/>
-            <Todolist truck={truck1} truck3={truck3} tasks={tasks2}/>
+            <Todolist title="What to learn" tasks={tasks1} />
+            <Todolist title="Songs" tasks={tasks2} />
         </div>
     );
 }
--- src/Todolist.tsx
+++ src/Todolist.tsx
@@ -1,46 +1,32 @@
 import React from 'react';
 
-type PropsType = {
-    truck: string
-    truck2?: number
-    truck3?: boolean
-    tasks: TaskType[]
-}
-
 type TaskType = {
     id: number
     title: string
     isDone: boolean
 }
 
-
-function Todolist(props: PropsType) {
-    return (
-        <div>
-            <div>
-                <h3>{props.truck}</h3>
-                <h3>{props.truck2}</h3>
-                <div>
-                    <input/>
-                    <button>+</button>
-                </div>
-                <ul>
-                    {props.tasks.map((el)=>{
-                        return(
-                            <li>
-                                <input type="checkbox" checked={el.isDone}/>
-                                <span>{el.title}</span></li>
-                        )
-                    })}
-                </ul>
-                <div>
-                    <button>All</button>
-                    <button>Active</button>
-                    <button>Completed</button>
-                </div>
-            </div>
-        </div>
-    );
+type PropsType = {
+    title: string
+    tasks: Array<TaskType>
 }
 
-export default Todolist;
+export function Todolist(props: PropsType) {
+    return <div>
+        <h3>{props.title}</h3>
+        <div>
+            <input/>
+            <button>+</button>
+        </div>
+        <ul>
+            <li><input type="checkbox" checked={props.tasks[0].isDone}/> <span>{props.tasks[0].title}</span></li>
+            <li><input type="checkbox" checked={props.tasks[1].isDone}/> <span>{props.tasks[1].title}</span></li>
+            <li><input type="checkbox" checked={props.tasks[2].isDone}/> <span>{props.tasks[2].title}</span></li>
+        </ul>
+        <div>
+            <button>All</button>
+            <button>Active</button>
+            <button>Completed</button>
+        </div>
+    </div>
+}
