687c0ce1602c96f1a032e366b88b9834fb4c80bd
Eugen <vasilenkoevgen88@gmail.com>
2023-09-21 18:21:01 +0300

    Preper for lesson 14

--- src/state/store.ts
+++ src/state/store.ts
@@ -1,15 +1,15 @@
-import {tasksReducer} from './tasks-reducer';
-import {todolistsRarneducer} from './todolists-reducer';
-import {combineReducers, createStore} from 'redux';
+import { tasksReducer } from './tasks-reducer';
+import { todolistsReducer } from './todolists-reducer';
+import {combineReducers, createStore, legacy_createStore} from 'redux';
 
 // объединяя reducer-ы с помощью combineReducers,
 // мы задаём структуру нашего единственного объекта-состояния
 const rootReducer = combineReducers({
     tasks: tasksReducer,
-    todolists: todolistsRarneducer
+    todolists: todolistsReducer
 })
 // непосредственно создаём store
-export const store = createStore(rootReducer);
+export const store = legacy_createStore(rootReducer);
 // определить автоматически тип всего объекта состояния
 export type AppRootStateType = ReturnType<typeof rootReducer>
 
