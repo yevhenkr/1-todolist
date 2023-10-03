456f971da28a3918059d1d8f8510e28d3bc3e97e
Eugen <vasilenkoevgen88@gmail.com>
2023-10-03 21:00:21 +0300

    add loder to App. During load card

--- src/app/app-reducer.ts
+++ src/app/app-reducer.ts
@@ -1,17 +1,26 @@
 export type RequestStatusType = 'idle' | 'loading' | 'succeeded' | 'failed'
 
 const initialState = {
-  status: 'idle' as RequestStatusType,
-  error: null as string | null,
+    status: 'idle' as RequestStatusType,
+    error: null as string | null,
 }
 
+type ActionsType = ReturnType<typeof setAppStatus>
 type AppStateType = typeof initialState
 
 export const appReducer = (state: AppStateType = initialState, action: ActionsType): AppStateType => {
-  switch (action.type) {
-    default:
-      return state
-  }
+    switch (action.type) {
+        case 'APP/STATUS':
+            return {
+                ...state,
+                status: action.status
+            }
+        default:
+            return state
+    }
 }
 
-type ActionsType = any
+export const setAppStatus = (status: RequestStatusType) => ({
+    type: "APP/STATUS" as const,
+    status,
+})
--- src/features/decks/AddNewDeckForm/AddNewDeckForm.tsx
+++ src/features/decks/AddNewDeckForm/AddNewDeckForm.tsx
@@ -1,49 +1,51 @@
 import s from './AddNewPostForm.module.css'
-import { useForm } from 'react-hook-form'
-import { useAppDispatch } from '../../../app/store.ts'
-import { addDeckTC } from '../decks-thunks.ts'
+import {useForm} from 'react-hook-form'
+import {useAppDispatch} from '../../../app/store.ts'
+import {addDeckTC, fetchDecksTC} from '../decks-thunks.ts'
 
 type FormValues = {
-  name: string
+    name: string
 }
 
 export const AddNewDeckForm = () => {
-  const dispatch = useAppDispatch()
+    const dispatch = useAppDispatch()
 
-  const {
-    register,
-    handleSubmit,
-    reset,
-    formState: { errors },
-  } = useForm<FormValues>({
-    defaultValues: {
-      name: '',
-    },
-  })
-
-  const onSubmit = (data: FormValues) => {
-    dispatch(addDeckTC(data.name)).then(() => {
-      reset()
+    const {
+        register,
+        handleSubmit,
+        reset,
+        formState: {errors},
+    } = useForm<FormValues>({
+        defaultValues: {
+            name: '',
+        },
     })
-  }
 
-  return (
-    <form className={s.form} onSubmit={handleSubmit(onSubmit)}>
-      <label className={s.label}>
-        Deck name
-        <input
-          {...register('name', {
-            required: 'Name is required',
-            minLength: {
-              value: 3,
-              message: 'Name must be longer than or equal to 3 characters',
-            },
-          })}
-          autoComplete="off"
-        />
-        <p className={s.errorMessage}>{errors.name && errors.name.message}</p>
-      </label>
-      <button type="submit">Add new deck</button>
-    </form>
-  )
+    const onSubmit = (data: FormValues) => {
+        dispatch(addDeckTC(data.name)).then(() => {
+            reset()
+        })
+    }
+    const addNewDeckHandler = () => {
+        dispatch(fetchDecksTC())
+    }
+    return (
+        <form className={s.form} onSubmit={handleSubmit(onSubmit)}>
+            <label className={s.label}>
+                Deck name
+                <input
+                    {...register('name', {
+                        required: 'Name is required',
+                        minLength: {
+                            value: 3,
+                            message: 'Name must be longer than or equal to 3 characters',
+                        },
+                    })}
+                    autoComplete="off"
+                />
+                <p className={s.errorMessage}>{errors.name && errors.name.message}</p>
+            </label>
+            <button onClick={addNewDeckHandler} type="submit">Add new deck</button>
+        </form>
+    )
 }
