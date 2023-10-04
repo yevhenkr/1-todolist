35847a9b5f56596f19f422f716bf79bed4d40fea
Eugen <vasilenkoevgen88@gmail.com>
2023-10-04 13:29:44 +0300

    "Render Props" Paterrn

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -4,14 +4,33 @@ import {GlobalError} from './GlobalError/GlobalError.tsx'
 import {useAppSelector} from "./store.ts";
 import {selectAppStatus} from "./app-selectors.ts";
 import {LinearLoader} from "../common/components/Loader/LinearLoader.tsx";
+import * as React from "react";
 
 export const App = () => {
     const appStatus = useAppSelector(selectAppStatus)
     return (
         <div>
+            <List item={["hyml", "frfr"]} renderItem={(item) => item.toUpperCase()}/>
+            <List item={[1.4, 2.5, 3]} renderItem={(item) => item.toFixed()}/>
+            <List item={[{name: "Mikola"}]} renderItem={(item) => item.name()}/>
             {appStatus === "loading" && <LinearLoader/>}
             <Decks/>
             <GlobalError/>
         </div>
+    )
+}
+
+type ListProps<T> = {
+    item: T[]
+    renderItem: (item: T) => React.ReactNode
+}
+
+function List<T>(props: ListProps<T>) {
+    return (
+        <ul>
+            {props.item.map((item, index) => (
+                <li key={index}>{props.renderItem(item)}</li>
+            ))}
+        </ul>
     )
 }
