2e0e10de912c93a03b6f4340fb41a5cde8d71fa4
Eugen <vasilenkoevgen88@gmail.com>
2023-10-04 10:17:47 +0300

    Disable buuton during loading an delete

--- src/features/decks/DecksList/DeckItem/DeckItem.tsx
+++ src/features/decks/DecksList/DeckItem/DeckItem.tsx
@@ -1,48 +1,56 @@
 import s from './DeckItem.module.css'
-import { useAppDispatch } from '../../../../app/store.ts'
-import { deleteDeckTC, updateDeckTC } from '../../decks-thunks.ts'
-import { Deck } from '../../decks-api.ts'
+import {useAppDispatch} from '../../../../app/store.ts'
+import {deleteDeckTC, updateDeckTC} from '../../decks-thunks.ts'
+import {Deck} from '../../decks-api.ts'
+import {useState} from "react";
 
 type DeckProps = {
-  deck: Deck
+    deck: Deck
 }
 
 const TEST_ACC_NAME = 'test'
 
-export const DeckItem = ({ deck }: DeckProps) => {
-  const isTestingDeck = deck.author.name === TEST_ACC_NAME
-  const dispatch = useAppDispatch()
+export const DeckItem = ({deck}: DeckProps) => {
+    const isTestingDeck = deck.author.name === TEST_ACC_NAME
+    const dispatch = useAppDispatch()
+    const [isLoading, setIsLoading] = useState<boolean>(false)
 
-  const handleDeleteButtonClick = () => {
-    dispatch(deleteDeckTC(deck.id))
-  }
+    const handleDeleteButtonClick = () => {
+        setIsLoading(true)
+        dispatch(deleteDeckTC(deck.id)).finally(() => {
+            setIsLoading(false)
+        })
+    }
 
-  const handleEditButtonClick = () => {
-    dispatch(updateDeckTC({ id: deck.id, name: `${deck.name} updated` }))
-  }
+    const handleEditButtonClick = () => {
+        setIsLoading(true)
+        dispatch(updateDeckTC({id: deck.id, name: `${deck.name} updated`})).finally(() => {
+            setIsLoading(false)
+        })
+    }
 
-  return (
-    <li className={s.item}>
-      <h3 className={s.title}>
-        {deck.name}
-        {isTestingDeck && '✨'}
-      </h3>
-      <p className={s.characteristic}>
-        <b>Author:</b> {deck.author.name}
-      </p>
-      <p className={s.characteristic}>
-        <b>Created:</b> {new Date(deck.created).toLocaleString('ru-Ru')}
-      </p>
-      <p className={s.characteristic}>
-        <b>Updated:</b> {new Date(deck.updated).toLocaleString('ru-Ru')}
-      </p>
+    return (
+        <li className={s.item}>
+            <h3 className={s.title}>
+                {deck.name}
+                {isTestingDeck && '✨'}
+            </h3>
+            <p className={s.characteristic}>
+                <b>Author:</b> {deck.author.name}
+            </p>
+            <p className={s.characteristic}>
+                <b>Created:</b> {new Date(deck.created).toLocaleString('ru-Ru')}
+            </p>
+            <p className={s.characteristic}>
+                <b>Updated:</b> {new Date(deck.updated).toLocaleString('ru-Ru')}
+            </p>
 
-      {isTestingDeck && (
-        <div className={s.buttonBox}>
-          <button onClick={handleEditButtonClick}>update</button>
-          <button onClick={handleDeleteButtonClick}>delete</button>
-        </div>
-      )}
-    </li>
-  )
+            {isTestingDeck && (
+                <div className={s.buttonBox}>
+                    <button onClick={handleEditButtonClick} disabled={isLoading}>update</button>
+                    <button onClick={handleDeleteButtonClick} disabled={isLoading}>delete</button>
+                </div>
+            )}
+        </li>
+    )
 }
